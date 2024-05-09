b89bcffb33c441f9700aa460b55a4553c1e309c0
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-05-09 18:27:31 +0200

    added Java Doc to BusinessImpl

--- wda-business/src/main/java/ch/hslu/informatik/swde/wda/business/BusinessImpl.java
+++ wda-business/src/main/java/ch/hslu/informatik/swde/wda/business/BusinessImpl.java
@@ -1,3 +1,11 @@
+/**
+ * Diese Schnittstelle gibt die Hauptfunktionalitäten vor, die für die
+ * Persistierung von Objekten benötigt werden.
+ *
+ * @author Kevin Forter
+ * @version 1.0
+ */
+
 package ch.hslu.informatik.swde.wda.business;
 
 import ch.hslu.informatik.swde.wda.domain.City;
@@ -20,46 +28,67 @@ public class BusinessImpl implements BusinessAPI {
     private static final WeatherDAO daoW = new WeatherDAOImpl();
     private static final ApiReader reader = new ApiReaderImpl();
 
+    /**
+     * Adds all cities to the database.
+     * <p>
+     * This method reads city details from an external source using the ApiReader.
+     * It then checks if the number of cities read is different from the number of cities currently in the database.
+     * If the numbers are different, it means there are new cities to be added, so it saves all the cities read into the database.
+     */
     @Override
     public void addAllCities() {
 
+        // Read city details from an external source
         LinkedHashMap<Integer, City> cityRes = reader.readCityDetailsList(reader.readCityNames());
 
+        // Check if the number of cities read is different from the number of cities currently in the database
         if (cityRes.size() != daoC.getNumberOfCities()) {
 
-            // cityRes als Batch speichern
+            // If there are new cities, save all the cities read into the database
             daoC.saveAllCities(cityRes);
         }
-
     }
 
+    /**
+     * Adds the current weather of a specified city to the database.
+     * <p>
+     * This method first retrieves the latest weather data of the specified city from both the database and an external API.
+     * If there is no existing weather data in the database for the city, it saves the current weather data from the API to the database.
+     * If there is existing weather data in the database, it checks if the timestamp of the latest weather data from the database is different from the current weather data from the API.
+     * If the timestamps are different, it calculates the time difference between the two timestamps.
+     * If the time difference is less than 40 minutes, it saves the current weather data from the API to the database.
+     * If the time difference is 40 minutes or more, it retrieves and saves the weather data of the city for the current year.
+     *
+     * @param cityName the name of the city for which the current weather data is to be added
+     */
     @Override
     public void addCurrentWeatherOfCity(String cityName) {
 
-        // Wetterdaten von DB und API abfragen
+        // Retrieve the latest weather data of the specified city from both the database and an external API
         Weather latestWeatherDAO = getLatestWeatherOfCity(cityName);
         Weather currentWeatherREADER = reader.readCurrentWeatherByCity(cityName);
 
         if (latestWeatherDAO == null) {
 
-            // Aktuelles Wetter von API holen und speichern
+            // If there is no existing weather data in the database for the city, save the current weather data from the API to the database
             currentWeatherREADER.setCityId(daoC.findCityIdByName(cityName));
             daoW.speichern(currentWeatherREADER);
 
         } else if (currentWeatherREADER != null && !latestWeatherDAO.getDTstamp().isEqual(currentWeatherREADER.getDTstamp())) {
 
-            // Zeitunterschied zwischen DB und API Wetter
+            // If there is existing weather data in the database, check if the timestamp of the latest weather data from the database is different from the current weather data from the API
+            // Calculate the time difference between the two timestamps
             Duration diff = Duration.between(latestWeatherDAO.getDTstamp(), currentWeatherREADER.getDTstamp());
 
             if (diff.toMinutes() < 40) {
 
-                // Aktuelles Wetter von API holen und speichern
+                // If the time difference is less than 40 minutes, save the current weather data from the API to the database
                 currentWeatherREADER.setCityId(latestWeatherDAO.getCityId());
                 daoW.speichern(currentWeatherREADER);
 
             } else {
 
-                // Wetter vom ganzen Jahr holen
+                // If the time difference is 40 minutes or more, retrieve and save the weather data of the city for the current year
                 addWeatherOfCityByYear(cityName, Year.now().getValue());
 
             }
@@ -67,49 +96,99 @@ public class BusinessImpl implements BusinessAPI {
     }
 
 
+    /**
+     * Adds the weather data of a specified city for a specific year to the database.
+     * <p>
+     * This method first checks if there are any existing weather data in the database for the specified city.
+     * If there are no existing weather data, it adds the current weather data of the city to the database.
+     * It then retrieves the weather data of the city for the specified year from an external API and the latest weather data of the city from the database.
+     * If the size of the weather data retrieved from the API is different from the number of weather data in the database for the city, it means there are new weather data to be added.
+     * So, it sets the city ID for each of the new weather data and saves them all to the database as a batch.
+     *
+     * @param cityName the name of the city for which the weather data is to be added
+     * @param year     the year for which the weather data is to be added
+     */
     @Override
     public void addWeatherOfCityByYear(String cityName, int year) {
 
-        // Überprüfe ob Wetterdaten vorhanden sind
+        // Check if there are any existing weather data in the database for the specified city
         if (!daoW.ifWeatherOfCityExist(cityName)) {
+            // If there are no existing weather data, add the current weather data of the city to the database
             addCurrentWeatherOfCity(cityName);
         }
 
-        // API call der Wetterdaten
+        // Retrieve the weather data of the city for the specified year from an external API
         TreeMap<LocalDateTime, Weather> weatherMap = reader.readWeatherByCityAndYear(cityName, year);
+        // Retrieve the latest weather data of the city from the database
         Weather latestWeather = getLatestWeatherOfCity(cityName);
 
+        // If the size of the weather data retrieved from the API is different from the number of weather data in the database for the city
         if (weatherMap.size() != daoW.getNumberOfWeatherByCity(cityName)) {
 
-            // CityId setzen
+            // Set the city ID for each of the new weather data
             for (Weather weather : weatherMap.values()) {
                 weather.setCityId(latestWeather.getCityId());
             }
 
-            // Als Batch speicher
+            // Save all the new weather data to the database as a batch
             daoW.saveAllWeather(weatherMap, cityName);
         }
     }
 
+    /**
+     * Retrieves a city object by its name.
+     * <p>
+     * This method uses the CityDAO to find a city by its name in the database.
+     * It returns the city object if found, otherwise it returns null.
+     *
+     * @param name the name of the city to be retrieved
+     * @return the city object if found, otherwise null
+     */
     @Override
     public City getCityByName(String name) {
-
         return daoC.findCityByName(name);
     }
 
+    /**
+     * Retrieves a list of all cities in the database.
+     * <p>
+     * This method uses the CityDAO to retrieve all cities from the database.
+     * It returns a list of city objects.
+     *
+     * @return a list of all city objects in the database
+     */
     @Override
     public List<City> getAllCities() {
-
         return daoC.alle();
     }
 
+    /**
+     * Retrieves the current weather of a specified city.
+     * <p>
+     * This method first finds the ID of the city by its name using the CityDAO.
+     * If the city ID is not 0, it retrieves the current weather of the city by its ID and name.
+     * If the city ID is 0, it returns a new weather object.
+     *
+     * @param cityName the name of the city for which the current weather is to be retrieved
+     * @return the current weather of the city if the city ID is not 0, otherwise a new weather object
+     */
     @Override
     public Weather getCurrentWeatherOfCity(String cityName) {
-
         int cityId = daoC.findCityIdByName(cityName);
         return cityId != 0 ? getCurrentWeatherOfCity(cityId, cityName) : new Weather();
     }
 
+    /**
+     * Retrieves the current weather of a specified city by its ID and name.
+     * <p>
+     * This method first retrieves the latest weather data of the city by its ID from the database and the current weather data of the city by its name from an external API.
+     * If the timestamps of the two weather data are the same, it returns the weather data from the database.
+     * If the timestamps are different, it adds the current weather data of the city to the database and retrieves it again.
+     *
+     * @param cityId   the ID of the city for which the current weather is to be retrieved
+     * @param cityName the name of the city for which the current weather is to be retrieved
+     * @return the current weather of the city
+     */
     private Weather getCurrentWeatherOfCity(int cityId, String cityName) {
 
         Weather daoWeather = daoW.findLatestWeatherByCity(cityId);
@@ -123,28 +202,63 @@ public class BusinessImpl implements BusinessAPI {
         }
     }
 
+    /**
+     * Retrieves the latest weather of a specified city by its name.
+     * <p>
+     * This method first finds the ID of the city by its name using the CityDAO.
+     * If the city ID is not 0, it retrieves the latest weather of the city by its ID.
+     * If the city ID is 0, it returns a new weather object.
+     *
+     * @param cityName the name of the city for which the latest weather is to be retrieved
+     * @return the latest weather of the city if the city ID is not 0, otherwise a new weather object
+     */
     @Override
     public Weather getLatestWeatherOfCity(String cityName) {
-
         int cityId = daoC.findCityIdByName(cityName);
         return cityId != 0 ? getLatestWeatherOfCity(cityId) : new Weather();
     }
 
+    /**
+     * Retrieves the latest weather of a specified city by its ID.
+     * <p>
+     * This method uses the WeatherDAO to find the latest weather of a city by its ID in the database.
+     * It returns the weather object if found.
+     *
+     * @param cityId the ID of the city for which the latest weather is to be retrieved
+     * @return the latest weather of the city
+     */
     private static Weather getLatestWeatherOfCity(int cityId) {
-
         return daoW.findLatestWeatherByCity(cityId);
-
     }
 
+    /**
+     * Retrieves the weather of a specified city for a specific year by its name.
+     * <p>
+     * This method first finds the ID of the city by its name using the CityDAO.
+     * If the city ID is not 0, it retrieves the weather of the city for the specified year by its ID.
+     * If the city ID is 0, it returns an empty TreeMap.
+     *
+     * @param year     the year for which the weather is to be retrieved
+     * @param cityName the name of the city for which the weather is to be retrieved
+     * @return a TreeMap of the weather of the city for the specified year if the city ID is not 0, otherwise an empty TreeMap
+     */
     @Override
     public TreeMap<LocalDateTime, Weather> getWeatherOfCityByYear(int year, String cityName) {
-
         int cityId = daoC.findCityIdByName(cityName);
         return cityId != 0 ? getWeatherOfCityByYear(year, cityId) : new TreeMap<>();
     }
 
+    /**
+     * Retrieves the weather of a specified city for a specific year by its ID.
+     * <p>
+     * This method uses the WeatherDAO to find the weather of a city for a specific year by its ID in the database.
+     * It returns a TreeMap of the weather if found.
+     *
+     * @param year   the year for which the weather is to be retrieved
+     * @param cityId the ID of the city for which the weather is to be retrieved
+     * @return a TreeMap of the weather of the city for the specified year
+     */
     private static TreeMap<LocalDateTime, Weather> getWeatherOfCityByYear(int year, int cityId) {
-
         return daoW.findWeatherFromCityByYear(year, cityId);
     }
 }
