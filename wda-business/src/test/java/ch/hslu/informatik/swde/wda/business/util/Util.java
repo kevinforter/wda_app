e4d63b459b0dfcdac690cfc11ba2c8908f94e167
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-05-15 16:27:02 +0200

    saving dummy obj

--- wda-business/src/test/java/ch/hslu/informatik/swde/wda/business/util/Util.java
+++ wda-business/src/test/java/ch/hslu/informatik/swde/wda/business/util/Util.java
@@ -1,9 +1,20 @@
 package ch.hslu.informatik.swde.wda.business.util;
 
+import ch.hslu.informatik.swde.wda.domain.City;
+import ch.hslu.informatik.swde.wda.domain.Weather;
+import ch.hslu.informatik.swde.wda.persister.DAO.CityDAO;
+import ch.hslu.informatik.swde.wda.persister.DAO.WeatherDAO;
+import ch.hslu.informatik.swde.wda.persister.impl.CityDAOImpl;
+import ch.hslu.informatik.swde.wda.persister.impl.WeatherDAOImpl;
 import ch.hslu.informatik.swde.wda.persister.util.JpaUtil;
 import jakarta.persistence.EntityManager;
 
+import java.time.LocalDateTime;
+
 public class Util {
+
+    private static final CityDAO daoC = new CityDAOImpl();
+    private static final WeatherDAO daoW = new WeatherDAOImpl();
 
     private Util() {
 
@@ -21,5 +32,14 @@ public class Util {
 
         em.close();
 
+    }
+
+    public static void saveDummyWeather() {
+
+        City dummyCity = new City(9999, "DUMMY", "DUMMY");
+        Weather dummyWeather = new Weather(daoC.findCityIdByName("DUMMY"), LocalDateTime.now().minusDays(1), "DUMMY", "DUMMY", 69.69, 69.69, 69.69, 69.69, 69.69);
+
+        daoC.speichern(dummyCity);
+        daoW.speichern(dummyWeather);
     }
 }
