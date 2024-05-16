5ea983d7b6d81f8ee3adc05e4d2a30ee1c4f37b4
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-05-16 23:37:46 +0200

    made print out prettier

--- wda-rws/src/main/java/ch/hslu/informatik/swde/wda/rws/util/ServerChecker.java
+++ wda-rws/src/main/java/ch/hslu/informatik/swde/wda/rws/util/ServerChecker.java
@@ -1,13 +1,14 @@
 package ch.hslu.informatik.swde.wda.rws.util;
 
 import java.net.Socket;
+import java.util.Arrays;
 
 public class ServerChecker {
     public static void checkServer(String host, int port) {
         try (Socket socket = new Socket(host, port)) {
-            System.out.println(host + " on PORT: " + port + " ✅");
+            System.out.printf("%-50s PORT: %d - Status: ✅%n", "Server Status: " + host, port);
         } catch (Exception e) {
-            System.out.println(host + " on PORT: " + port + " ❌");
+            System.out.printf("%-50s PORT: %d - Status: ❌%n", "Server Status: " + host, port);
         }
     }
 }
